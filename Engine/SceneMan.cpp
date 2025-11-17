78c56cc025dcd3bfdb41201d6dc0fd99dc67faaa
ZBlackstock <40592313@live.napier.ac.uk>
2025-11-17 16:25:49 +0000

    setup most of cpp

--- Engine/SceneMan.cpp
+++ Engine/SceneMan.cpp
@@ -1,13 +1,57 @@
 #include <memory>
 #include <vector>
+#include <iostream>
 #include "SceneMan.hpp"
 
-std::shared_ptr<Scene> SceneManager::get_active_scene()
+using sm = SceneManager;
+using s = Scene;
+
+// _______________________ Scene Manager ______________________________________________
+
+sm::init()
+{
+	// Change to have custom scene classes or die
+	int i = 0;
+	while (i < 5) // Temp loop to handle multiple scenes
+	{
+		std::shared_ptr<Scene> scene = std::make_shared<Scene>();
+		scenes.push_back(scene);
+		i++;
+	}
+
+	sm::set_active_scene(scenes[0]);
+}
+
+// Calls update for active_scene
+sm::update(const float& dt)
+{
+	active_scene->update(dt);
+}
+
+std::shared_ptr<Scene> sm::get_active_scene()
 {
 	return active_scene;
 }
 
-void SceneManager::set_active_scene(std::shared_ptr<Scene> scene)
+void sm::set_active_scene(std::shared_ptr<Scene> scene)
 {
+	cout << "active_scene = " << scene << endl;
 	active_scene = scene;
 }
+
+// _______________________________ Scene ______________________________________________
+
+s::Scene(std::string scene_name)
+{
+	name = scene_name;
+}
+
+s::update(const float& dt) // Can probably remove this, as update will be hanled by derived classes
+{	
+}
+
+// ____________________________ Custom Scenes _________________________________________
+
+// NEED:
+// main_menu
+// map
--- Engine/SceneMan.hpp
+++ Engine/SceneMan.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <string>
 #include <memory> // For pointers
 #include "RenderMan.h"
 
@@ -9,9 +10,9 @@ public:
 	std::vector<std::shared_ptr<Scene>> scenes;
 
 	void init() override; // Stores all scenes, calls init for all scenes too
-	void update(const float* dt); // Calls update for active_scene
+	void update(const float &dt); // Calls update for active_scene
 
-	set_active_scene(std::shared_ptr<Scene> scene); // Change to new scene
+	void set_active_scene(std::shared_ptr<Scene> scene); // Change to new scene
 	std::shared_ptr<Scene> get_active_scene(); // Returns active_scene
 
 private:
@@ -21,9 +22,12 @@ private:
 class Scene
 {
 public:
+	Scene(string scene_name);
+
+	std::string name;
 	RenderMan* render_manager;
 	virtual void init(); // Derived class will load sprites and logic specific to itself
-	virtual void update(const float* dt);
+	virtual void update(const float &dt);
 	virtual void on_scene_active(); // Called when scene is set to active_scene, add SceneMan drawables
 	virtual void on_scene_inactive(); // Called on frame when no lonhger active_scene, clear SceneMan drawables
 };
