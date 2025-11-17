dcf588aa212b3857842ba07157e3b9070cc4af13
ZBlackstock <40592313@live.napier.ac.uk>
2025-11-17 15:11:25 +0000

    Declared vector of shared pointer to scenes, and active scene

--- Engine/SceneMan.hpp
+++ Engine/SceneMan.hpp
@@ -1,6 +1,15 @@
+#include <vector>
+#include <memory> // For pointers
 
+class SceneManager
+{
+public:
+	std::vector<std::shared_ptr<Scene>> scenes;
+	std::shared_ptr<Scene> active_scene;
 
-class SceneManager 
+};
+
+class Scene
 {
 
 };
