25f285b6ca8b971dbe4a2cfe248109be5a2ad835
ZBlackstock <40592313@live.napier.ac.uk>
2025-11-19 18:01:42 +0000

    setup constructor

--- Engine/UI_button.cpp
+++ Engine/UI_button.cpp
@@ -1,14 +1,25 @@
 #include "UI_button.hpp"
 #include "RenderMan.h"
+#include <SFML/Graphics.hpp>
+#include "game_system.h"
 #include <iostream>
+#include <string>
 
 using b = Button;
+using rm = RenderMan;
+using gs = GameSystem;
 
-b::Button(std::string name_in_file, Button above, Button Below) 
+b::Button(std::string name_in_file, Button above, Button Below, sf::Vector2f location, int sort_layer)
 {
 	try
 	{
-		
+		Button b::_above = above;
+		Button b::_below = below;
+
+		RenderMan::create_sprite(gs::sprites_path + name_in_file + "_idle", { 10000,10000 }, sort_layer);
+		RenderMan::create_sprite(gs::sprites_path + name_in_file + "_highlighted", { 10000,10000 }, sort_layer);
+		RenderMan::create_sprite(gs::sprites_path + name_in_file + "_selected", { 10000,10000 }, sort_layer);
+		RenderMan::create_sprite(gs::sprites_path + name_in_file + "_disabled", { 10000,10000 }, sort_layer);
 	}
 	catch
 	{
--- Engine/UI_button.hpp
+++ Engine/UI_button.hpp
@@ -4,18 +4,10 @@
 class Button
 {
 public:
-	Button(std::string name_in_file, Button above, Button Below);
+	Button(std::string name_in_file, Button above, Button Below, sf::Vector2f location, int sort_layer);
 	~Button() = default;
 
 private:
 	Button _above;
 	Button _below;
-
-	// Strings, because they are grabbed as sprites via filenames
-	// Private, because they should all be under same name
-	// Example: name = "play". These should be named in file as "play_idle", "play_highlighted" etc.
-	std::string idle;
-	std::string highlighted;
-	std::string selected;
-	std::string disabled;
 };
