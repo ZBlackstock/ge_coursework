f00edd6d230d121449fce80b5767628422afeafd
ZBlackstock <40592313@live.napier.ac.uk>
2025-11-30 23:41:25 +0000

    replaced init with constructor

--- healthbar.cpp
+++ healthbar.cpp
@@ -2,12 +2,18 @@
 
 using h = Healthbar;
 
+h::Healthbar(float& max)
+{
+	*_max_value = max;
+	RenderMan::createDrawable(sf::RectangleShape(_max_value), 3)
+}
+
 void h::init()
 {
-
 }
 
 void h::set_healthbar_value(float& value)
 {
-
+	*_value = value;
+	
 }
--- healthbar.hpp
+++ healthbar.hpp
@@ -1,13 +1,16 @@
 #pragma once
 #include <memory>
+#include <SFML/Graphics.hpp>
+#include "Engine/RenderMan.h"
 
 
 class Healthbar
 {
 public:
-	void init();
+	Healthbar(float& max);
 	void set_healthbar_value(float& value);
 
 private:
-	std::make_shared<float> _value = std::make_shared<float>();
+	std::shared_ptr<float> _value = std::make_shared<float>();
+	std::shared_ptr<float> _max_value = std::make_shared<float>();
 };
